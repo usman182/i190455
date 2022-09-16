0f6af821eec63df333439f18c9c32e7fa8fd2736
usman182 <i190455@nu.edu.pk>
2022-09-16 18:09:37 +0500

    First Activity Comit

--- app/src/main/java/com/muhammadusman/i190455/MainActivity.java
+++ app/src/main/java/com/muhammadusman/i190455/MainActivity.java
@@ -2,35 +2,32 @@ package com.muhammadusman.i190455;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
-    ImageView img;
-    TextView txt;
+    Timer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        img = findViewById(R.id.musifyimage);
-        txt = findViewById(R.id.musifytext);
-
-        img.setOnClickListener(new View.OnClickListener() {
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
             @Override
-            public void onClick(View view) {
-                setContentView(R.layout.activity_2);
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, activity_2.class);
+                startActivity(intent);
+                finish();
             }
-        });
+        }, 1500);
 
-        txt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setContentView(R.layout.activity_2);
-            }
-        });
     }
 }
